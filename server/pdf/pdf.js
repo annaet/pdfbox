081c4efdeba28d37ade53105be063f8dc8c4fde6
Anna Thomas <annathom@uk.ibm.com>
2017-01-17 13:50:04 +0000

    Updated pdfbox to run as child process

--- server/pdf/pdf.js
+++ server/pdf/pdf.js
@@ -1,7 +1,6 @@
 var https = require('https');
 var fs = require('fs');
-var Document = require('node-pdfbox');
-var PDFImage = require("pdf-image").PDFImage;
+var path = require('path');
 
 var download = function(url, dest, cb) {
   'use strict';
@@ -27,7 +26,7 @@ var getPage = function (req, res) {
   if (req.body.origin) {
     var destination = req.body.destination;
     if (!destination) {
-      destination = 'page.pdf';
+      destination = 'paper.pdf';
     }
 
     download(req.body.origin, destination, function(err) {
@@ -39,31 +38,38 @@ var getPage = function (req, res) {
       pageNum = parseInt(pageNum, 10);
 
       if (!Number.isInteger(pageNum)) {
-        pageNum = 0;
+        pageNum = 1;
       }
 
-      var pdfImage = new PDFImage(destination);
-      pdfImage.convertPage(pageNum).then(function (imagePath) {
-        res.download(imagePath, function(err) {
+      var paperPath = path.join(__dirname, '../..', destination);
+      var imagePath = path.join(__dirname, '../..', 'image');
+      var imageName = imagePath + pageNum + '.jpg';
+
+      var spawn = require('child_process').spawn;
+      var child = spawn('java', ['-jar', '-Xmx2g', 'pdfbox/pdfbox-app-2.0.4.jar', 'PDFToImage', '-imageType', 'jpg', '-startPage', pageNum, '-endPage', pageNum, '-resolution', '120', paperPath, '-outputPrefix', imagePath]);
+
+      child.stdout.on('data', function(data) {
+        console.log('stdout: ' + data);
+      });
+
+      child.stderr.on('data', function(data) {
+        console.log('stderr: ' + data);
+      });
+
+      child.on('close', function(code) {
+        if (code !== 0) {
+          return console.log('error: child process exited with code ' + code);
+        }
+
+        res.download(imageName, function(err) {
           if (err) {
             console.log(err);
           }
 
-          fs.unlink(destination);
-          fs.unlink(imagePath);
+          fs.unlink(paperPath);
+          fs.unlink(imageName);
         });
-      }, function (err) {
-        console.log(err);
-        res.status(500).send(err);
       });
-
-      // var document = Document.loadSync(destination);
-      // var page = document.getPageSync(pageNum);
-      // console.log(document.pagesCountSync());
-      // page.extractSync(destination);
-
-      // var filePath = '/../../' + destination;
-      // res.download(__dirname + filePath);
 
     });
   } else {
